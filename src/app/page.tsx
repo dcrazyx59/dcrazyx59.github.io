7f624db8563d248e924d7619b24ca4c013616782
Adrian Chapple <adriaaan.matrix@hotmail.com>
2025-03-11 23:49:49 +0100

    Firt commit, dependencies

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+// layout => display => spacing => border => colors => text
+
+import { useState } from "react";
+
+export default function Home() {
+  const [namePerson, setNamePerson] = useState<string>("Adrian")
+
+  const handleNamePerson = (person: string) => {
+    if (person === "Adrian") return setNamePerson("Javier") 
+    return setNamePerson("Adrian") 
+  }
+
+  return (
+    <div className="flex flex-col justify-center text-center h-screen">
+      <span className="align-middle hover:text-primary-light hover:text-2xl">Hello my name is {namePerson}</span>
+      <div>
+        <button type="button" onClick={() =>handleNamePerson(namePerson)} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 m-3">Click me!</button>
+      </div>
+      
+    </div>
+    );
+}
