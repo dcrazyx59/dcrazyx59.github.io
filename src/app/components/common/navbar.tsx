14bf91222b5b64a95dfbbedacdbb42b28b650c59
Adrian Chapple <adriaaan.matrix@hotmail.com>
2025-03-19 00:48:34 +0100

    Navbar prototype added

--- src/app/components/common/navbar.tsx
+++ src/app/components/common/navbar.tsx
@@ -1,7 +1,46 @@
-export default function NavBar() {
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+  
     return (
-        <main>
-            This is my NavBar
-        </main>
-    )
-}
+        <nav className="p-2 pt-8 border-b-1 ">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href="/" className=" text-lg font-bold">
+                    AppIcon
+                </Link>
+
+                {/* Mobile Menu Button */}
+                <button
+                    onClick={toggleMenu}
+                    className="text-white md:hidden focus:outline-none"
+                >
+                ☰
+                </button>
+
+                {/* Desktop Menu */}
+                <div className="hidden md:flex space-x-30 text-2xl">
+                    <Link href="#" className=" hover:text-gray-400 ">Home</Link>
+                    <Link href="/about" className=" hover:text-gray-400">About</Link>
+                    <Link href="/tech" className=" hover:text-gray-400">Technologies</Link>
+                    <Link href="/contact" className=" hover:text-gray-400">Contact Me</Link>
+                </div>
+
+                {/* Mobile Menu (Dropdown) */}
+                {isMenuOpen && (
+                <div className="md:hidden absolute top-16 right-0 bg-gray-800 w-full">
+                    <Link href="#" className="block p-2 hover:text-gray-400 ">Home</Link>
+                    <Link href="/about" className="block p-2 hover:text-gray-400">About</Link>
+                    <Link href="/tech" className="block p-2 hover:text-gray-400">Technologies</Link>
+                    <Link href="/contact" className="block p-2 hover:text-gray-400">Contact Me</Link>
+                </div>
+                )}
+            </div>
+        </nav>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,16 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col justify-center text-center h-screen">
+    <div className="h-screen">
       <NavBar/>
-      <Counter/>
-      <span className="align-middle hover:text-primary-light">Hello my name is {namePerson}</span>
-      <div>
-        <button type="button" onClick={() =>handleNamePerson(namePerson)} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 m-3">Click me!</button>
+      <div className="flex flex-col justify-center align-middle m-auto text-center">
+        <Counter/>
+        <span className="align-middle hover:text-primary-light">Hello my name is {namePerson}</span>
+        <div>
+          <button type="button" onClick={() =>handleNamePerson(namePerson)} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 m-3">Click me!</button>
+        </div>
+        <span></span>
       </div>
-      <span></span>
       
     </div>
     );
