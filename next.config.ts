8719a08fb2b25429cbace0bdf652ee89c39a431e
Adrian Chapple <adriaaan.matrix@hotmail.com>
2025-04-05 22:16:38 +0200

    Changed paths for deploy

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   devIndicators: false,
-  basePath: process.env.NODE_ENV === 'production' ? '/dcrazyx59.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dcrazyx59.github.io' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
